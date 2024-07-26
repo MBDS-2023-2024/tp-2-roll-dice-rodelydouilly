274b14c7c02e3fed31ca9a6bcbcc723dda45c9ff
Rodely Douilly <rodelydouilly@gmail.com>
2024-07-26 11:30:03 -0400

    Etape 2 Transformer l'application en Casino

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -8,6 +8,10 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     @SuppressLint("ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,24 +20,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        // Create new Dice objects with 6 sides and roll them
+        val dice1 = Dice(6)
+        val diceRoll1 = dice1.roll()
 
+        val dice2 = Dice(6)
+        val diceRoll2 = dice2.roll()
+
+        // Update the screen with the dice rolls
+        val resultTextView1: TextView = findViewById(R.id.textView1)
+        resultTextView1.text = diceRoll1.toString()
+
+        val resultTextView2: TextView = findViewById(R.id.textView2)
+        resultTextView2.text = diceRoll2.toString()
+
+        // Check if the user wins
+        if (diceRoll1 == diceRoll2) {
+            Toast.makeText(this, "Félicitations ! Vous avez gagné !", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
-class Dice (private val numSlides: Int){
+// Class Dice with private variable
+class Dice(private val numSides: Int) {
 
+    // Random function roll()
     fun roll(): Int {
-        return (1 ..numSlides).random()
+        return (1..numSides).random()
     }
 
 }
